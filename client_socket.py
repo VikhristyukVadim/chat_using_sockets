4dd3fe505ee4d4b10c1b80650cca972eb87cc10f
VikhristyukVadim <vadim.3vium@gmail.com>
2021-12-15 12:08:19 +0300

    Application using sockets, first commit2

--- client_socket.py
+++ client_socket.py
@@ -1,29 +1,51 @@
 from main_socket_file import Socket
-from threading import Thread
+import asyncio
+from datetime import datetime
+from os import system
 
 
 class Client(Socket):
     def __init__(self):
         super(Client, self).__init__()
 
+        self.messages = ""
+
     def set_up(self):
-        self.connect(('localhost', 55555))
-        listen_thread = Thread(target=self.listen_socket)
-        listen_thread.start()
+        try:
+            self.socket.connect(('127.0.0.1', 55555))
+        except ConnectionRefusedError or ConnectionError:
+            print("Server is offline")
+            exit(0)
 
-        send_thread = Thread(target=self.send_data, args=(None,))
-        send_thread.start()
+        self.socket.setblocking(False)
 
-    def listen_socket(self, listened_socket=None):
+    async def listen_socket(self, listened_socket=None):
         while True:
-            data = self.recv(1024)
-            print(data.decode("utf-8"))
+            data = await self.main_loop.sock_recv(self.socket, 1024)
+            self.messages += f"{datetime.now().date()} [{datetime.now().strftime('%H:%M:%S')}] ->" \
+                             f" {data.decode('utf-8')} \n"
 
-    def send_data(self, data):
+            system("clear")
+            print(self.messages)
+
+    async def send_data(self, data=None):
         while True:
-            self.send(input(">>>").encode("utf-8"))
+            data = await self.main_loop.run_in_executor(None, input)
+            await self.main_loop.sock_sendall(self.socket, data.encode('utf-8'))
+
+    async def main(self):
+        listening_task = self.main_loop.create_task(self.listen_socket())
+        sending_task = self.main_loop.create_task(self.send_data())
+        #
+        # await asyncio.gather(
+        #     self.main_loop.create_task(self.listen_socket()),
+        #     self.main_loop.create_task(self.send_data())
+        # )
+        return await asyncio.gather(listening_task, sending_task, return_exceptions=True)
 
 
 if __name__ == '__main__':
     client = Client()
     client.set_up()
+
+    client.start()
--- server_socket.py
+++ server_socket.py
@@ -1,43 +1,60 @@
 from main_socket_file import Socket
-from threading import Thread
+import asyncio
+import socket
 
 
 class Server(Socket):
     def __init__(self):
         super(Server, self).__init__()
 
-        self.listen(10)
-        print('Server is running, please, press ctrl+c to stop')
         self.users = []
 
     def set_up(self):
-        self.bind(('', 55555))
-        self.accept_sockets()
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(('127.0.0.1', 55555))
+        self.socket.listen(10)
+        print('Server is running, please, press ctrl+c to stop')
+        self.socket.setblocking(False)
 
-    def send_data(self, data):
+    async def send_data(self, data):
         for user in self.users:
-            user.send(data)
+            await self.main_loop.sock_sendall(user, data)
 
-    def listen_socket(self, listened_socket=None):
-        print("listening user")
+    async def listen_socket(self, listened_socket=None):
+
+        if not listened_socket:
+            return
+
         while True:
-            data = listened_socket.recv(1024)
-            print(f'user send {str(data)}')
 
-            self.send_data(data)
+            data = await self.main_loop.sock_recv(listened_socket, 1024)
 
-    def accept_sockets(self):
+            await self.send_data(data)
+            # try:
+            #     print("listening user", listened_socket)
+            #     data = await self.main_loop.sock_recv(listened_socket, 1024)
+            #
+            #     # print(f'user send {str(data).encode("utf-8")}')
+            #     await self.send_data(data)
+            # except ConnectionResetError:
+            #     print("Client removed")
+            #     self.users.remove(listened_socket)
+            #     return
+
+    async def accept_sockets(self):
         while True:
-            user_socket, addr = self.accept()
-            print(f'User < {addr[0]} > connected', )
+            user_socket, addr = await self.main_loop.sock_accept(self.socket)
+            print(f'User < {addr[0]} > connected')
 
             self.users.append(user_socket)
+            self.main_loop.create_task(self.listen_socket(user_socket))
 
-            listen_accepted_user = Thread(target=self.listen_socket, args=(user_socket,))
-
-            listen_accepted_user.start()
+    async def main(self):
+        await self.main_loop.create_task(self.accept_sockets())
 
 
 if __name__ == '__main__':
     server = Server()
     server.set_up()
+
+    server.start()
